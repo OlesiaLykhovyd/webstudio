69f3219486f81e31a9052dcc4cfb90cd1c5b135a
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2022-03-16 15:22:46 +0100

    stop body scroll when modal is open

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,23 @@
 (() => {
-const refs = {
+  const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-};
+    body: document.querySelector('body'),
+  };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-function toggleModal() {
+  function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
-}
+
+    if (!refs.modal.classList.contains('is-hidden')) {
+      // Disable scroll
+      refs.body.style.overflow = 'hidden';
+    } else {
+      // Enable scroll
+      refs.body.style.overflow = 'auto';
+    }
+  }
 })();
